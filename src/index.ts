80c8cc8cfb0eece300014bde88e76c4a5221f07d
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-07-31 19:47:02 +0300

    work on index ts

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,23 @@ const matches = fs.readFileSync('football.csv', {
 .map((row: string): string[] => {
   return row.split(',');
 });
-console.log(matches); 
+
+const MatchesResult = {
+  HomeWin: 'H',
+  AwayWin: 'A',
+  Draw: 'D'
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchesResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchesResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
+
+ 
