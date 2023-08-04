ff85d9c795ec0ea1fa1e92371e8f9dc98b347637
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-08-04 19:03:54 +0300

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader('football.csv');
-reader.read();
+//create an object that satisfies DataReader interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// create an instance of MatchReader and pass in smth satisfying DataReader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+//matchReader.matches
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
