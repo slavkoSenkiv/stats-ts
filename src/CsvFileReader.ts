2bf82dd7b8ae02073cabc4e94e3b6c2d9abdcc20
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-08-05 08:02:59 +0300

    finish CsvFilereader: add mappint and type conversion to row value, finish index ts to make it count actual Man United won game stats

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -2,11 +2,10 @@ import fs from 'fs';
 import { MatchResults } from './MatchResults';
 import { dateStringToDate } from './utils';
 
-
 type MatchData = [Date, string, string, number, number, MatchResults, string];
 
 export class CsvFileReader {
-  data: string[][] = []//MatchData[] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string){}
 
@@ -19,8 +18,16 @@ export class CsvFileReader {
     .map((row: string): string[] => {
       return row.split(',');
     })
-    /* .map((row: string): MatchData => {
-      return []
-    }) */
+    .map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResults,
+        row[6]
+      ];
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResults } from "./MatchResults";
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
-console.log(reader.data);
 
+let countWins = 0;
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin){
+    countWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin){
+    countWins++;
+  }
+}
 
-
+console.log(`Man United won ${countWins} games`);
 
