f3db6166168d1ccbfd94357f74771e1a1503fb66
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-08-19 07:46:11 +0200

    build summary class with analyzer and output classes

--- src/Summary.ts
+++ src/Summary.ts
@@ -12,5 +12,10 @@ export class Summary {
   constructor(
     public analyzer: Analyzer,
     public outputTarget: OutputTarget) {}
+
+    buildAndPrintReport(matches: MatchData[]): void {
+      const output = this.analyzer.run(matches);
+      this.outputTarget.print(output);
+    }
 }
 
