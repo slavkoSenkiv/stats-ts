e75ea7faefb84bde43423bba384f17622217d2c9
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-08-19 07:40:55 +0200

    separate report class

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ const csvFileReader = new CsvFileReader('football.csv');
 // create an instance of MatchReader and pass in smth satisfying DataReader interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-//matchReader.matches
+//matchReader.matches   
  
